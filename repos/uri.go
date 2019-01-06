a1e765231badd148f92d1d0cb27caf98e56a36ac
Tom Wiesing <tkw01536@gmail.com>
2019-01-06 15:45:15 +0100

    Add mechanism for canonical urls

--- repos/uri.go
+++ repos/uri.go
@@ -69,6 +69,16 @@ func NewRepoURI(s string) (repo *RepoURI, err error) {
 	return
 }
 
+// IsCanonicalURI checks if a given URI is in canonical form
+// using a specific canonical specification
+func IsCanonicalURI(s string, cspec string) bool {
+	uri, err := NewRepoURI(s)
+	if err != nil {
+		return false
+	}
+	return uri.Canonical(cspec) == s
+}
+
 var schemeRegex = regexp.MustCompile("^[a-zA-Z][a-zA-Z0-9+\\-\\.]*$")
 
 // validateScheme validates a scheme
@@ -116,4 +126,46 @@ func (rURI *RepoURI) Components() (parts []string) {
 	}
 
 	return
+}
+
+var specReplace = regexp.MustCompile("[\\^\\%]")
+
+// Canonical returns the canonical version of this URI given a canonical specification
+// the canonical specification can contain any character, except for three special ones
+// ^ -- replaced by the first un-used component of the URI
+// % -- replaced by the second un-used component of the URI (commonly username)
+// $ -- replaced by all remaining components in the URI joined with a '/'. Also stops all processing afterwards
+// If $ does not exist in the cspec, it is assumed to be at the end of the cspec.
+func (rURI *RepoURI) Canonical(cspec string) (canonical string) {
+	// get the components of the URI
+	components := rURI.Components()
+
+	// split into prefix and suffix
+	prefix, suffix := utils.SplitAfter(cspec, "$")
+
+	prefix = specReplace.ReplaceAllStringFunc(prefix, func(s string) string {
+		// if everything is empty, return the string as is
+		if len(components) == 0 {
+			return s
+		}
+
+		// replace by the first component
+		if s == "^" {
+			defer func() { components = components[1:] }()
+			return components[0]
+		}
+
+		// else we want to replace by the second component
+		// so we need to make sure we have that many
+		if len(components) < 2 {
+			return s
+		}
+
+		// do the replacement
+		defer func() { components = append(components[:1], components[2:]...) }()
+		return components[1]
+	})
+
+	// add the remaining components
+	return prefix + strings.Join(components, "/") + suffix
 }
