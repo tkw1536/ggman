ce34c9c845ebbcd753f16851a1a253447fe1fc39
Tom Wiesing <tkw01536@gmail.com>
2020-09-13 15:29:12 +0200

    Use gogenlicense for license information
    This commit moves to using 'gogenlicense' for generating legal notices.

--- constants/legal/docs.go
+++ constants/legal/docs.go
@@ -1,4 +1,4 @@
 // Package legal contains notices about software included inside the ggman program.
 package legal
 
-//go:generate bash make_license_notices.sh github.com/tkw1536/ggman legal notices.go Notices
+//go:generate gogenlicense -p legal -n Notices -d notices.go github.com/tkw1536/ggman
