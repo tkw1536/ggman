b1f0b4cfe6a30761b8f49e5eb6cc6ede3435651a
Tom Wiesing <tkw01536@gmail.com>
2021-04-08 20:22:31 +0200

    program/GetMainOpts: Slight optimisation

--- program/usage.go
+++ program/usage.go
@@ -60,7 +60,7 @@ func GetMainOpts(opt *Description) (opts []usagefmt.Opt) {
 	}
 
 	// local options were requested, so we need to exclude depending on the configuration.
-	skipLongNames := make([]string, 0, 2)
+	skipLongNames := make([]string, 0, 3)
 	if !opt.Environment.AllowsFilter {
 		skipLongNames = append(skipLongNames, "for", "here", "no-fuzzy-filter")
 	}
