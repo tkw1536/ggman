98d1ba7f7d6e242b1949374f6800b9b4fb9fcd51
Tom Wiesing <tkw01536@gmail.com>
2022-02-20 06:51:19 +0100

    env: Move making a filter into package
    This commit moves makes a filter into the env package.

--- env/flags.go
+++ env/flags.go
@@ -1,6 +1,14 @@
 package env
 
-// Flags represent flags for the main ggman program
+import (
+	"github.com/pkg/errors"
+	"github.com/tkw1536/ggman/internal/walker"
+	"github.com/tkw1536/ggman/program"
+	"github.com/tkw1536/ggman/program/exit"
+	"github.com/tkw1536/ggman/program/meta"
+)
+
+// Flags represents a set of filter flags used for the ggman program.
 type Flags struct {
 	Filters       []string `short:"f" long:"for" value-name:"filter" description:"Filter list of repositories to apply COMMAND to by filter. Filter can be a relative or absolute path, or a glob pattern which will be matched against the normalized repository url"`
 	NoFuzzyFilter bool     `short:"n" long:"no-fuzzy-filter" description:"Disable fuzzy matching for filters"`
@@ -16,4 +24,112 @@ type Flags struct {
 
 	Tarnished bool `short:"t" long:"tarnished" description:"List only repositories which are dirty or unsynced"`
 	Pristine  bool `short:"p" long:"pristine" description:"List only repositories which are clean and synced"`
+}
+
+var errNotADirectory = exit.Error{
+	ExitCode: ExitInvalidRepo,
+	Message:  "Not a directory: %q",
+}
+
+// NewFilter creates a new filter corresponding to the given Flags and Environment.
+func NewFilter(flags Flags, env *Env) (filter Filter, err error) {
+	// generate pattern filters for the "--for" arguments
+	clauses := make([]Filter, len(flags.Filters))
+	for i, pat := range flags.Filters {
+
+		// check if 'pat' represents the root of a repository
+		if repo, err := env.AtRoot(pat); err == nil && repo != "" {
+			clauses[i] = PathFilter{Paths: []string{repo}}
+			continue
+		}
+
+		// create a normal pattern filter
+		clauses[i] = NewPatternFilter(pat, !flags.NoFuzzyFilter)
+	}
+
+	// here filter: alias for --path .
+	if flags.Here {
+		flags.Path = append(flags.Path, ".")
+	}
+
+	// for each of the candidate paths, add a path filter
+	pf := PathFilter{Paths: make([]string, len(flags.Path))}
+	for i, p := range flags.Path {
+		var err error
+		pf.Paths[i], _, err = env.At(p) // try to use the current repository first.
+		if err != nil {
+			// filter sub-repositories under this repo!
+			pf.Paths[i], err = env.Abs(p)
+			if err != nil {
+				return nil, errors.Wrapf(err, "Unable to resolve path: %q", p)
+			}
+
+			// make sure it is actually a directory!
+			if ok, err := walker.IsDirectory(pf.Paths[i], true); err != nil || !ok {
+				return nil, errNotADirectory.WithMessageF(p)
+			}
+		}
+	}
+
+	if len(pf.Paths) > 0 {
+		clauses = append(clauses, pf)
+	}
+
+	// only set the filter when we actually have something to filter by
+	filter = DisjunctionFilter{Clauses: clauses}
+	if len(clauses) == 0 {
+		filter = NoFilter
+	}
+
+	// add a WorktreeFilter filter if requested
+	if flags.Dirty || flags.Clean {
+		filter = WorktreeFilter{
+			Filter: filter,
+
+			Dirty: flags.Dirty,
+			Clean: flags.Clean,
+		}
+	}
+
+	if flags.Synced || flags.UnSynced {
+		filter = StatusFilter{
+			Filter: filter,
+
+			Synced:   flags.Synced,
+			UnSynced: flags.UnSynced,
+		}
+	}
+
+	if flags.Tarnished || flags.Pristine {
+		filter = TarnishFilter{
+			Filter: filter,
+
+			Tarnished: flags.Tarnished,
+			Pristine:  flags.Pristine,
+		}
+	}
+
+	return
+}
+
+// Requirement represents a set of requirements on the Environment.
+type Requirement struct {
+	// Does the environment require a root directory?
+	NeedsRoot bool
+
+	// Does the environment allow filtering?
+	// AllowsFilter implies NeedsRoot.
+	AllowsFilter bool
+
+	// Does the environment require a CanFile?
+	NeedsCanFile bool
+}
+
+// AllowsFlag checks if the provided option is allowed by this option
+func (req Requirement) AllowsFlag(flag meta.Flag) bool {
+	return req.AllowsFilter
+}
+
+func (req Requirement) Validate(args program.Arguments[Flags]) error {
+	return program.ValidateAllowedFlags[Flags](req, args)
 }
--- runtime.go
+++ runtime.go
@@ -1,112 +1,24 @@
 package ggman
 
 import (
-	"github.com/pkg/errors"
 	"github.com/tkw1536/ggman/env"
-	"github.com/tkw1536/ggman/internal/walker"
-	"github.com/tkw1536/ggman/program/exit"
 )
 
 // NewRuntime makes a new runtime for ggman
 func NewRuntime(params env.Parameters, context Context) (*env.Env, error) {
 	// create a new environment
-	env, err := env.NewEnv(context.Description.Requirements, params)
+	e, err := env.NewEnv(context.Description.Requirements, params)
 	if err != nil {
 		return nil, err
 	}
 
-	// make a filter
-	f, err := makeFilter(env, context.Args)
+	// setup a filter for it!
+	f, err := env.NewFilter(context.Args.Flags, e)
 	if err != nil {
 		return nil, err
 	}
-	env.Filter = f
+	e.Filter = f
 
-	return env, nil
+	return e, nil
 
-}
-
-var errNotADirectory = exit.Error{
-	ExitCode: env.ExitInvalidRepo,
-	Message:  "Not a directory: %q",
-}
-
-func makeFilter(e *env.Env, c Arguments) (env.Filter, error) {
-	// generate pattern filters for the "--for" arguments
-	clauses := make([]env.Filter, len(c.Flags.Filters))
-	for i, pat := range c.Flags.Filters {
-
-		// check if 'pat' represents the root of a repository
-		if repo, err := e.AtRoot(pat); err == nil && repo != "" {
-			clauses[i] = env.PathFilter{Paths: []string{repo}}
-			continue
-		}
-
-		// create a normal pattern filter
-		clauses[i] = env.NewPatternFilter(pat, !c.Flags.NoFuzzyFilter)
-	}
-
-	// here filter: alias for --path .
-	if c.Flags.Here {
-		c.Flags.Path = append(c.Flags.Path, ".")
-	}
-
-	// for each of the candidate paths, add a path filter
-	pf := env.PathFilter{Paths: make([]string, len(c.Flags.Path))}
-	for i, p := range c.Flags.Path {
-		var err error
-		pf.Paths[i], _, err = e.At(p) // try to use the current repository first.
-		if err != nil {
-			// filter sub-repositories under this repo!
-			pf.Paths[i], err = e.Abs(p)
-			if err != nil {
-				return nil, errors.Wrapf(err, "Unable to resolve path: %q", p)
-			}
-
-			// make sure it is actually a directory!
-			if ok, err := walker.IsDirectory(pf.Paths[i], true); err != nil || !ok {
-				return nil, errNotADirectory.WithMessageF(p)
-			}
-		}
-	}
-
-	if len(pf.Paths) > 0 {
-		clauses = append(clauses, pf)
-	}
-
-	// only set the filter when we actually have something to filter by
-	var dj env.Filter = env.DisjunctionFilter{Clauses: clauses}
-	if len(clauses) == 0 {
-		dj = env.NoFilter
-	}
-
-	// add a WorktreeFilter filter if requested
-	if c.Flags.Dirty || c.Flags.Clean {
-		dj = env.WorktreeFilter{
-			Filter: dj,
-
-			Dirty: c.Flags.Dirty,
-			Clean: c.Flags.Clean,
-		}
-	}
-
-	if c.Flags.Synced || c.Flags.UnSynced {
-		dj = env.StatusFilter{
-			Filter: dj,
-
-			Synced:   c.Flags.Synced,
-			UnSynced: c.Flags.UnSynced,
-		}
-	}
-
-	if c.Flags.Tarnished || c.Flags.Pristine {
-		dj = env.TarnishFilter{
-			Filter: dj,
-
-			Tarnished: c.Flags.Tarnished,
-			Pristine:  c.Flags.Pristine,
-		}
-	}
-
-	return dj, nil
 }
