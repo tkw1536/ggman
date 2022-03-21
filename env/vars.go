12b2e6d31651bd24ffbd131fb932acc6012674e6
Tom Wiesing <tkw01536@gmail.com>
2022-03-21 12:46:18 +0100

    env/vars: Use reflect to read variables
    This commit updates env/vars to not hard-code the variables names to be
    read from the environment. Instead it uses reflect to read them from
    appropriate struct tags.

--- env/vars.go
+++ env/vars.go
@@ -2,6 +2,7 @@ package env
 
 import (
 	"os"
+	"reflect"
 
 	"github.com/mitchellh/go-homedir"
 )
@@ -11,30 +12,57 @@ import (
 //
 // This object is used to prevent code in ggman to access the environment directly, which is difficult to test.
 // Instead access goes through this layer of indirection which can be mocked during testing.
+//
+// The env struct-tag indidicates which environment variable the value corresponds to.
 type Variables struct {
 	// HOME is the path to the users' home directory
 	// This is typically stored in the 'HOME' variable on unix-like systems
 	HOME string
 
 	// PATH is the value of the 'PATH' environment variable
-	PATH string
+	PATH string `env:"PATH"`
 
 	// GGROOT is the value of the 'GGROOT' environment variable
-	GGROOT string
+	GGROOT string `env:"GGROOT"`
 
 	// CANFILE is the value of the 'GGMAN_CANFILE' environment variable
-	CANFILE string
+	CANFILE string `env:"GGMAN_CANFILE"`
 
 	// GGNORM is the value of the 'GGNORM' environment variable
-	GGNORM string
+	GGNORM string `env:"GGNORM"`
 }
 
-// ReadVariables reads a new variables instances from the environment
+// variableEnvNames holds a mapping from reflect-field-indexes in Variables to os.GetEnv() names
+var variablesEnvNames map[int]string
+
+func init() {
+	// iterate over all the fields for Variables
+	rT := reflect.TypeOf((*Variables)(nil)).Elem()
+	nF := rT.NumField()
+	variablesEnvNames = make(map[int]string, nF)
+	for i := 0; i < nF; i++ {
+		// check if we have the `env` tag
+		// and store it in variablesEnvNames
+		env, ok := rT.Field(i).Tag.Lookup("env")
+		if !ok {
+			continue
+		}
+		variablesEnvNames[i] = env
+	}
+}
+
+// ReadVariables reads Variables from the operating system
 func ReadVariables() (v Variables) {
-	v.HOME, _ = homedir.Dir() // errors result in an empty home value
-	v.CANFILE = os.Getenv("GGMAN_CANFILE")
-	v.GGROOT = os.Getenv("GGROOT")
-	v.PATH = os.Getenv("PATH")
-	v.GGNORM = os.Getenv("GGNORM")
+	// assign the os.Getenv() values
+	rV := reflect.ValueOf(&v).Elem()
+	for i, env := range variablesEnvNames {
+		value := os.Getenv(env)
+		rV.Field(i).SetString(value)
+	}
+
+	// set the HOME variable
+	// errors result in an empty home
+	v.HOME, _ = homedir.Dir()
+
 	return
 }
