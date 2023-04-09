ffbfc8a22b8a8882dd8ca78e5fbe94ba4272a99b
Tom Wiesing <tkw01536@gmail.com>
2023-04-09 13:56:11 +0200

    Reimplement some shell aliases
    This commit simplifies the implementation of some of the shell aliases.

--- cmd/shellrc.sh
+++ cmd/shellrc.sh
@@ -5,7 +5,7 @@
 # This also works with short names, e.g. "ggcd world" will cd into the first
 # repository matching "world".
 ggcd () {
-	ggman -f "$1" ls --exit-code --one && cd "$(ggman -f "$1" ls --exit-code --one 2>&1)"
+	REPO="$(ggman -f "$1" ls --exit-code --one 2>&1)" && echo "$REPO" && cd "$REPO"
 }
 
 # ggshow is like ggcd, except that is displays the 
@@ -15,7 +15,7 @@ ggshow () {
 
 # ggcode is like ggcd, except it opens an editor (here vscode) instead of cding. 
 ggcode () {
-	ggman -f "$1" ls --exit-code --one && code "$(ggman -f "$1" ls --exit-code --one 2>&1)"
+	REPO="$(ggman -f "$1" ls --exit-code --one 2>&1)" && echo "$REPO" && code "$REPO"
 }
 
 # ggclone clones a repository if it does not yet exist, and then ccds into the correct directory.
