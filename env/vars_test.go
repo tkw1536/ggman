bd3678d45c068139a4f02346903c3f3332670e72
Tom Wiesing <tkw01536@gmail.com>
2024-12-08 19:30:29 +0100

    replace testutil.MockVariables function
    The function testutil.MockVariables was only used in a single place.
    This commit creates a local function instead, and removes it from
    testutil package.

--- env/vars_test.go
+++ env/vars_test.go
@@ -2,23 +2,22 @@ package env
 
 //spellchecker:words reflect testing github ggman internal testutil
 import (
+	"os"
 	"reflect"
 	"testing"
-
-	"github.com/tkw1536/ggman/internal/testutil"
 )
 
 //spellchecker:words GGROOT CANFILE GGNORM
 
 func TestReadVariables(t *testing.T) {
-	defer testutil.MockVariables(map[string]string{
-		"PATH":          "/fake/path",
-		"HOME":          "/fake/home",
-		"USERPROFILE":   "/fake/home",
-		"GGROOT":        "/fake/ggroot",
-		"GGMAN_CANFILE": "/fake/canfile",
-		"GGNORM":        "something-fake",
-	})()
+
+	// set fake environment variables for test
+	defer setenv(t, "PATH", "/fake/path")()
+	defer setenv(t, "HOME", "/fake/home")()
+	defer setenv(t, "USERPROFILE", "/fake/home")()
+	defer setenv(t, "GGROOT", "/fake/ggroot")()
+	defer setenv(t, "GGMAN_CANFILE", "/fake/canfile")()
+	defer setenv(t, "GGNORM", "something-fake")()
 
 	got := ReadVariables()
 	want := Variables{
@@ -31,5 +30,22 @@ func TestReadVariables(t *testing.T) {
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("ReadVariables() = %v, want %v", got, want)
+	}
+}
+
+// sets an environment variable, and returns a function to clean up
+func setenv(t *testing.T, name, value string) func() {
+	old := os.Getenv(name)
+
+	// set new value
+	if err := os.Setenv(name, value); err != nil {
+		t.Errorf("failed to set environment variable %q: %s", name, err)
+	}
+
+	// return a function to revert to the old value
+	return func() {
+		if err := os.Setenv(name, old); err != nil {
+			t.Errorf("failed to set environment variable %q: %s", name, err)
+		}
 	}
 }
