9ab862ebe9ee2c3a73d914f82ac7d070a923d401
Tom Wiesing <tkw01536@gmail.com>
2025-04-01 22:09:56 +0200

    env/canfile: Add test for reading file

--- env/canfile_test.go
+++ env/canfile_test.go
@@ -3,6 +3,7 @@ package env
 //spellchecker:words reflect testing
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -30,6 +31,56 @@ func TestCanLine_UnmarshalText(t *testing.T) {
 			}
 			if !reflect.DeepEqual(cl, tt.wantCl) {
 				t.Errorf("CanLine.Unmarshal() = %v, want %v", cl, tt.wantCl)
+			}
+		})
+	}
+}
+
+func TestCanFile_ReadFrom(t *testing.T) {
+	tests := []struct {
+		name    string
+		src     string
+		wantCF  CanFile
+		wantErr bool
+	}{
+		{
+			name:    "empty",
+			src:     "",
+			wantCF:  CanFile(nil),
+			wantErr: false,
+		},
+		{
+			name: "canfile with several lines",
+			src: `
+# for anything on git.example.com, clone with https
+^git.example.com https://$.git
+
+# for anything on git2.example.com leave the urls unchanged
+^git2.example.com $$
+
+# by default, clone via ssh
+git@^:$.git
+`,
+			wantCF: CanFile{
+				CanLine{Pattern: "^git.example.com", Canonical: "https://$.git"},
+				CanLine{Pattern: "^git2.example.com", Canonical: "$$"},
+				CanLine{Pattern: "", Canonical: "git@^:$.git"},
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var cf CanFile
+
+			_, gotErr := cf.ReadFrom(strings.NewReader(tt.src))
+			if (gotErr != nil) != tt.wantErr {
+				t.Errorf("CanFile.ReadFrom() error = %v, wantErr %v", gotErr, tt.wantErr)
+			}
+
+			if !reflect.DeepEqual(cf, tt.wantCF) {
+				t.Errorf("CanLine.ReadFrom() = %#v, want %#v", cf, tt.wantCF)
 			}
 		})
 	}
