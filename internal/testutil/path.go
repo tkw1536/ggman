fa057c2352edb23d014f5d726e5a18971e369b63
Tom Wiesing <tkw01536@gmail.com>
2025-05-20 18:45:52 +0200

    Revert "testutil: Minor performance improvement"
    This reverts commit d1412740f45543e63a4976954114ac5b11c64d99.

--- internal/testutil/path.go
+++ internal/testutil/path.go
@@ -18,11 +18,11 @@ var defaultVolumeName = filepath.VolumeName(os.TempDir())
 //
 // When path starts with "/", the path is guaranteed to contain a volume name.
 func ToOSPath(path string) (result string) {
-	path = filepath.ToSlash(path)
 	if len(path) > 0 && path[0] == '/' {
-		return defaultVolumeName + path
+		result = defaultVolumeName
 	}
-	return path
+	result += filepath.FromSlash(path)
+	return result
 }
 
 // ToOSPaths is like ToOSPath, but applies to each value in a slice or array.
