d29679d9f86174f8c32b33d2d1af7dacf81323df
Tom Wiesing <tkw01536@gmail.com>
2025-07-16 08:49:08 +0200

    shellrc: Use long form arguments
    This commit improves the maintainability of the `shellrc` script by
    using long form arguments instead of the short form arguments.

--- cmd/shellrc.sh
+++ cmd/shellrc.sh
@@ -5,22 +5,22 @@
 # This also works with short names, e.g. "ggcd world" will cd into the first
 # repository matching "world".
 ggcd () {
-	REPO="$(ggman -f "$1" ls --exit-code --one 2>&1)" && echo "$REPO" && cd "$REPO"
+	REPO="$(ggman --for "$1" ls --exit-code --one 2>&1)" && echo "$REPO" && cd "$REPO"
 }
 
 # ggshow is like ggcd, except that is displays the 
 ggshow () {
-	REPO="$(ggman -f "$1" ls --exit-code --one 2>&1)" && ggman -f "$REPO" show --no-patch 2>&1
+	REPO="$(ggman --for "$1" ls --exit-code --one 2>&1)" && ggman --for "$REPO" show --no-patch 2>&1
 }
 
 # ggcode is like ggcd, except it opens an editor (here vscode) instead of cding. 
 ggcode () {
-	REPO="$(ggman -f "$1" ls --exit-code --one 2>&1)" && echo "$REPO" && code "$REPO"
+	REPO="$(ggman --for "$1" ls --exit-code --one 2>&1)" && echo "$REPO" && code "$REPO"
 }
 
 # ggclone clones a repository if it does not yet exist, and then ccds into the correct directory.
 ggclone () {
-	DEST="$(ggman --no-fuzzy-filter -f "$1" ls --one)"
+	DEST="$(ggman --no-fuzzy-filter --for "$1" ls --one)"
 	if [ "$DEST" = "" ]; then
 		ggman clone "$@" || return $?
 		DEST="$(ggman where "$1")"
