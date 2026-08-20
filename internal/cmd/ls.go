33dc11338fec91d197f98536c94b3d825a498429
Tom Wiesing <tkw01536@gmail.com>
2026-08-20 09:46:52 +0200

    Migrate to encoding/json/v2
    This commit migrates from the plain "encoding/json" to the new
    "encoding/json/v2" package.

--- internal/cmd/ls.go
+++ internal/cmd/ls.go
@@ -2,7 +2,8 @@ package cmd
 
 //spellchecker:words encoding json path filepath sync essio shellescape github cobra ggman internal pkglib collection exit
 import (
-	"encoding/json"
+	"encoding/json/jsontext"
+	"encoding/json/v2"
 	"fmt"
 	"path/filepath"
 	"sync"
@@ -217,9 +218,10 @@ func (l *ls) outputExport(cmd *cobra.Command, repos []Repo) error {
 }
 
 func (l *ls) outputJSON(cmd *cobra.Command, repos []Repo) error {
-	encoder := json.NewEncoder(cmd.OutOrStdout())
-	encoder.SetIndent("", "  ")
-	if err := encoder.Encode(repos); err != nil {
+	if err := json.MarshalWrite(
+		cmd.OutOrStdout(), repos,
+		jsontext.WithIndent(" "), json.Deterministic(true),
+	); err != nil {
 		return fmt.Errorf("%w: %w", errGenericOutput, err)
 	}
 	return nil
